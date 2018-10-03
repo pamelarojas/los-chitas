692e68ec26f4e929f43352ba265fcc9d8ad93eff
unknown <p.rojasbaquedano@gmail.com>
2018-10-03 15:55:55 -0300

    cambios2

--- los-chitas/page-noticias.php
+++ los-chitas/page-noticias.php
@@ -5,59 +5,57 @@
   <hr>
   <h1 class="mt-5 mb-5">Noticias</h1>
   <hr>
-  <div class="container noticias1">
+  <div class="container noticias_1">
     <div class="row">
-      <div class="noticias1 col-12 col-sm-12 col-md-6 col-lg-6 pl-5 pr-5 mt-5 mb-5">
+      <div class="foto_1 col-12 col-sm-12 col-md-6 col-lg-6 pl-5 pr-5 mt-5 mb-5">
         <img src="<?php echo get_theme_file_uri() ?>/assets/img/familia.jpg" class="img-fluid" alt=""style="border-radius: 30px;">
       </div>
       <hr>
-      <div class="noticias_1 col-12 col-sm-12 col-md-6 col-lg-6 pl-5 pr-5 mt-5 mb-5">
+      <div class="texto_1 col-12 col-sm-12 col-md-6 col-lg-6 pl-5 pr-5 mt-5 mb-5">
         <h1 class="noticias-title">Más que un Club...</h1>
         <p class="noticias-text">Día de Reciclaje!!! apoderados y deportistas participaron de un hermoso día de reciclaje y juegos.
         El fin de la Actividad es reunir dinero para financiar algunos viajes del club y diversas actividades.</p>
       </div>
-      <hr>
-      
       <hr>
       <div class="noticias1">
         <div class="noticias_1 col-12 col-sm-12 col-md-6 pl-5 pr-5 mt-5 mb-5">
           <h1 class="noticias-title">Campeonato Cono Sur 2018</h1>
           <p class="noticias-text">Este sábado 28 de Julio nuestro club participó con un grupo de nuestros deportistas en el Campeonato Cono Sur en Neuquén, Argentina. Con una presentación excelente y de gran nivel de parte de ellos, logrando el 3° lugar en la categoría de velocidad. Felicitamos a nuestra deportista Antonia Mariqueo Olmedo y su familia que la apoyan día a día en este gran deporte.</p>
         </div>
-        
+
         <div class="noticias1 col-12 col-sm-12 col-md-6  pr-5 mt-5 mb-5">
           <img src="<?php echo get_theme_file_uri() ?>/assets/img/galeria2.jpg" class="img-fluid" alt="" style="border-radius: 30px; height: 400px;">
         </div>
       </div>
-    </div> 
+    </div>
   </div>
-</section> 
-
-<hr>
+</section>
+<hr>     
 
 <section>
-  <div class="container">
-    <div class="row noticias2">
-      <div class="noticias_2 col-12 col-sm-12 col-md-6 col-lg-6 pl-5 pr-5 mt-5 mb-5">
+  <div class="noticias2">
+    <div class="row">
+      <div class="foto_2 col-12 col-sm-12 col-md-6 col-lg-6 pl-5 pr-5 mt-5 mb-5">
         <img src="<?php echo get_theme_file_uri() ?>/assets/img/logo-1.jpg" class="img-fluid" alt="" style="border-radius: 100%;">
       </div>
       <hr>
-      <div class="noticias_1 col-12 col-sm-12 col-md-6 col-lg-6 pl-5 pr-5 mt-5 mb-5">
+      <div class="texto_2 col-12 col-sm-12 col-md-6 col-lg-6 pl-5 pr-5 mt-5 mb-5">
         <h1 class="noticias-title">Nuestro Primer Logo</h1>
         <p class="noticias-text">Este fue Nuestro primer logo, el cual en un principio Represento todas las garras que queríamos poner en nuestro club, con los años tomamos la decisión de renovarlo ya que habían compañeros que lo encontraban muy agresivo.</p>
       </div>
+    </div>
+  </div>
+  <hr>
 
+  <div class="container noticias_2"> 
+    <div class="row">
+      <div class="noticias_1 col-12 col-sm-12 col-md-6 col-lg-6 pl-5 pr-5 mt-5 mb-5">
+        <h1 class="noticias-title">Logo Actual</h1>
+        <p class="noticias-text">Nuestro segundo y actual logo. Luego de tomar la decisión a nivel de club, deportistas y sus familias llegamos a la conclusión de cambiar el logo por uno que representara más el actual momento del club, y fuera más amistoso para nuestra competencia</p>
+      </div>
       <hr>
-
-      <div class="noticias2">
-        <div class="noticias_1 col-12 col-sm-12 col-md-6 col-lg-6 pl-5 pr-5 mt-5 mb-5">
-          <h1 class="noticias-title">Logo Actual</h1>
-          <p class="noticias-text">Nuestro segundo y actual logo. Luego de tomar la decisión a nivel de club, deportistas y sus familias llegamos a la conclusión de cambiar el logo por uno que representara más el actual momento del club, y fuera más amistoso para nuestra competencia</p>
-        </div>
-        <hr>
-        <div class="noticias_2 col-12 col-sm-12 col-md-6 col-lg-6 pl-5 pr-5 mt-5 mb-5">
-          <img src="<?php echo get_theme_file_uri() ?>/assets/img/chitas.jpg" class="img-fluid" alt=""style="border-radius: 50%;width: 400px;">
-        </div>
+      <div class="noticias_2 col-12 col-sm-12 col-md-6 col-lg-6 pl-5 pr-5 mt-5 mb-5">
+        <img src="<?php echo get_theme_file_uri() ?>/assets/img/chitas.jpg" class="img-fluid" alt=""style="border-radius: 50%;width: 400px;">
       </div>
     </div>
   </div>
@@ -70,13 +68,13 @@
     <div class="noticias_2 col-12 col-sm-12 col-md-6 col-lg-6 mt-5 mb-5">
       <img src="<?php echo get_theme_file_uri() ?>/assets/img/patines-.jpg" class="img-fluid" alt="Imagen patines" style="border-radius: 10px;">
     </div>
-    <hr>
+    <hr> 
     <div class=" noticias_1 col-12 col-sm-12 col-md-6 col-lg-6">
       <h1 class="text-center mt-5">Únete...<i class="far fa-smile-wink" style="font-size: 50px; color: red;"></i></h1>
-    </div>  
+    </div>
   </div>
 </section> 
 
-<hr>    
+
 
 <?php get_footer(); ?>
